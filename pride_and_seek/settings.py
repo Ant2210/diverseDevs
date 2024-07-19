e0f9085bd2c48db1db56db87262dde3042e1f3b1
Oliver Sharp <olliesharp5@gmail.com>
2024-07-19 13:50:42 +0000

    Added home, about, map and blog apps

--- pride_and_seek/settings.py
+++ pride_and_seek/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
+    'about',
+    'blog',
+    'map',
 ]
 
 MIDDLEWARE = [
