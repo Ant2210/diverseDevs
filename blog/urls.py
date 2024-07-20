0032a0d9583dc33ffc61266dc5fa41f652651393
Oliver Sharp <olliesharp5@gmail.com>
2024-07-20 20:08:55 +0000

    Added functionality for users to create posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import blog, admin_posts, community_posts
+from .views import blog, admin_posts, community_posts, create_post
 
 urlpatterns = [
     path('', blog, name='blog'),
     path('admin-posts/', admin_posts, name='admin_posts'),
     path('community-posts/', community_posts, name='community_posts'),
+    path('create-post/', create_post, name='create_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import Post
+from profiles.models import UserProfile
+from .forms import PostForm
 
 def blog(request):
     """ A view to return the blog.html page """
@@ -8,6 +12,8 @@ def blog(request):
         'admin_posts_url': reverse('admin_posts'),
         'community_posts_url': reverse('community_posts'),
     }
+    if request.user.is_authenticated:
+        context['form'] = PostForm()
     return render(request, 'blog/blog.html', context)
 
 def admin_posts(request):
@@ -17,3 +23,18 @@ def admin_posts(request):
 def community_posts(request):
     community_posts = Post.objects.filter(post_type='community')
     return render(request, 'blog/community_posts.html', {'posts': community_posts})
+
+@login_required
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = UserProfile.objects.get(user=request.user)
+            post.post_type = 'community'  # Set post_type to 'community'
+            post.save()
+            messages.success(request, 'You have successfully contributed to our blog! Please see the community posts tab to view your contribution!')
+            return redirect('blog')
+    else:
+        form = PostForm()
+    return render(request, 'blog/create_post.html', {'form': form})
