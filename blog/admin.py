da381d2ed71efd6dda9444e94e8ce9d3951d3b48
Oliver Sharp <olliesharp5@gmail.com>
2024-07-21 14:34:52 +0000

    Added new structure to Post model to account for blog structure

--- blog/admin.py
+++ blog/admin.py
@@ -5,4 +5,14 @@ from .models import Post
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'post_type', 'created_at')
     list_filter = ('post_type', 'created_at')
-    search_fields = ('title', 'content')
+    search_fields = ('title', 'header1', 'content1', 'header2', 'content2', 'header3', 'content3')
+    fields = (
+        'title', 
+        'image', 
+        'header1', 'content1', 
+        'header2', 'content2', 
+        'header3', 'content3', 
+        'header4', 'content4',
+        'post_type', 
+        'author'
+    )
